10668f932b0a1ad3459454e4f53db7dc5e5c9123
Vasanth Naik <12rvnaik@gmail.com>
2023-02-02 23:23:11 +0530

    new file:   Learn/AlgoForCP/Algebra/BinaryExponenation.cpp 	new file:   Questions/1Math/ProblemsName.txt 	new file:   Questions/2Bit/ProblemsName.txt 	new file:   Questions/3Recursion/ProblemsName.txt 	new file:   Questions/4Array/ProblemsName.txt 	new file:   Questions/5Searching/ProblemsName.txt

---
+++ Learn/AlgoForCP/Algebra/BinaryExponenation.cpp
@@ -0,0 +1,19 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll bxp(ll x, ll y)
+{
+    ll ans=1;
+    while(y > 0)
+    {
+        if(y & 1)
+        {
+            ans = ans * x;
+        }
+        y = y/2;
+        x = x*x;
+    }
+    return ans;
+}
