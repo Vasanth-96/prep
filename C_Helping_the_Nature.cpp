d1989345ef2fa91d35a835944ddadd16e7886170
Vasanth Naik <12rvnaik@gmail.com>
2023-03-17 02:42:59 +0530

    deleted:    .cph/.A_Is_It_a_Cat.cpp_398fe7940dcd8b10e50e24be91f2358b.prob 	deleted:    .cph/.A_Lame_King.cpp_98003ae4c65b56fdbba895f98f4e3b4e.prob 	deleted:    .cph/.A_Prefix_and_Suffix_Array.cpp_fd6e387ae32f4b2641f5f5dc259b52a8.prob 	new file:   .cph/.B_Circle_Game.cpp_6fd1448779ae073a96ffaf47b61d27ee.prob 	new file:   .cph/.B_Palindromic_Numbers.cpp_b1b09b6216a4361153e538045d9507c4.prob 	new file:   .cph/.B_Palindromic_Numbers.py_18d00a3881f7e8a090d52a5fbf380ff6.prob 	deleted:    .cph/.B_Patchouli_s_Magical_Talisman.cpp_9dfd7f86b7efd36601917078741f8f5c.prob 	deleted:    .cph/.B_Settlement_of_Guinea_Pigs.cpp_1da1e23b94413086e0406c5c7a27cd22.prob 	deleted:    .cph/.B_Vaccination.cpp_c9210638fa9ffa610e7339d8f22b9f98.prob 	new file:   .cph/.C_Helping_the_Nature.cpp_51fd3824d58815500ab09fa84a0e093d.prob 	deleted:    .cph/.C_Manipulating_History.cpp_164d437cd4be9d344115a8f6d816be27.prob 	deleted:    .cph/.C_Pull_Your_Luck.cpp_13ff21e4ab5cea42abf65a34e8f3cefa.prob 	deleted:    .cph/.C_Scoring_Subsequences.cpp_5f9db33daed4006bae0b50536d59a8ba.prob 	deleted:    .cph/.C_The_Very_Beautiful_Blanket.cpp_e6e29cc5c0fe94abceea9f01a52c8b28.prob 	new file:   .cph/.C_Zero_Path.cpp_33b3ce96bf87a4c87f7c746c45d93bc4.prob 	deleted:    .cph/.D_Black_and_White_Stripe.cpp_1cb206cfc6cbbdfad0480a2f51995cc8.prob 	deleted:    .cph/.D_Buying_gifts.cpp_fa6c5fcdb5661595adeea8a08e4501fb.prob 	deleted:    .cph/.D_The_Enchanted_Forest.cpp_b28d2761885022a40abe1595373a5b9b.prob 	deleted:    .cph/.E_2_Unforgivable_Curse_hard_version.cpp_c403251f34b781e658b9b52912eb5d8d.prob 	deleted:    .cph/.E_Price_Maximization.cpp_13a90277d4f9d21adaca2d548d921454.prob 	deleted:    .cph/.Permutation_Subsequence.cpp_54ef1495916ae1a749fba46aee89921f.prob 	modified:   .vscode/settings.json 	deleted:    B_Vaccination.cpp 	new file:   C_Helping_the_Nature.cpp

---
+++ C_Helping_the_Nature.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+const int M = 1e9 + 7;
+#define pb push_back
+typedef long long ll;
+#define endl '\n'
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+}
