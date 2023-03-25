5457cefe62cf360977fa40b3fd1692e8baf0af8c
Vasanth Naik <12rvnaik@gmail.com>
2023-03-25 15:59:45 +0530

    new file:   .cph/.1_Guess_the_Number.cpp_f4021c281eb58d1496da37e37d78b305.prob 	new file:   .cph/.A_Garland.cpp_c18bf83e5f694cfe73faf2560dfd5604.prob 	new file:   .cph/.A_Hossam_and_Combinatorics.cpp_301f34fd3b3506413ea57bbaff87be1d.prob 	new file:   .cph/.A_K_divisible_Sum.cpp_c768dfaa9b912cda3602584f7c56fe32.prob 	new file:   .cph/.B_Different_Divisors.cpp_603fcae2bd11f8014253419e014dc325.prob 	new file:   .cph/.B_Hossam_and_Friends.cpp_cdcfc44a876c9634e6d7f9f482926b08.prob 	new file:   .cph/.B_Points_on_Plane.cpp_9c764961e438d7ce4b765d9702667b88.prob 	new file:   .cph/.B_Points_on_Plane.py_b4eb2c51c60eb5126cb4a4a9b7be623e.prob 	new file:   .cph/.C_2_Powering_the_Hero_hard_version.cpp_4b112eea899c86d68cb8ada2d68d2a1a.prob 	new file:   .cph/.C_Dijkstra.cpp_110e697fb3d9b58761a596f973e9de69.prob 	new file:   .cph/.C_Slay_the_Dragon.cpp_5f0fb669aecc1b1ee22715531ec58cb8.prob 	new file:   .cph/.C_Sum_on_Subarrays.cpp_3d6ff32d8aec6f6df1ea41c121e8f178.prob 	modified:   .cph/.E_Interview.cpp_5cc24c55b4dbd77d6aade1027af5e488.prob 	modified:   .vscode/settings.json 	modified:   E_Interview.cpp 	new file:   Questions/5Searching/tworepeatedelements.cpp 	new file:   Questions/6Sorting/ProblemsName.txt 	new file:   Questions/8Matrix/ProblemsName.txt

--- E_Interview.cpp
+++ E_Interview.cpp
@@ -4,26 +4,13 @@ using namespace std;
 const int M = 1e9 + 7;
 #define pb push_back
 typedef long long ll;
-#define endl '\n'
+#define endl '\n' // comment in case of non-interactive
 
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
-        }
-        
-        cout.flush();
-    }   
+       
     
 }
