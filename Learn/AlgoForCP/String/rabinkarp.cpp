84aff203a74bac2a8e9a875ce1898cabbf082bd1
Vasanth Naik <12rvnaik@gmail.com>
2023-04-19 16:25:47 +0530

    updated

--- Learn/AlgoForCP/String/rabinkarp.cpp
+++ Learn/AlgoForCP/String/rabinkarp.cpp
@@ -4,13 +4,14 @@ using namespace std;
 const int M = 1e9 + 7;
 #define pb push_back
 typedef long long ll;
-#define endl '\n' // comment in case of non-interactive
+#define endl '\n' // comment in case of interactive and (dont forget) use immediately after any cout 
+
 
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-       
+    
     
 }
